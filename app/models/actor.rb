fc9bfb14749df6e16ece9feeedda797b9c797247
Yuliya Marinova <yuliyambg@gmail.com>
2020-10-09 03:48:40 +0000

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
   
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+
+def list_roles
+    self.characters.collect do |character|
+     "#{character.name} - #{character.show.name}"
+    end.join
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
   
+  def actors_list
+  actors.collect do |actor|
+  actor.full_name
+   end
+  end
+
 end
